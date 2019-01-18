c59d387dca47bbad329ffd4c91c057da976a3e8f
PC <julya.p.dongmail.com>
2019-01-18 20:57:55 +0200

    Replaced use of threads to thread pools.

---
+++ TaskExecutor/Executor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+
+namespace TaskExecutor
+{
+    public class Executor
+    {
+        private readonly ConcurrentQueue<Action> _tasksQueue = new ConcurrentQueue<Action>();
+        
+        /// <summary>
+        /// Provides number of tasks in the queue.
+        /// </summary>
+        public int NumberOfTasks
+        {
+            get { return _tasksQueue.Count(); }
+        }
+
+        /// <summary>
+        /// Adds task into the queue to be executed.
+        /// </summary>
+        /// <param name="task">Task to execute</param>
+        public void AddToExecutionQueue(Action task)
+        {
+            _tasksQueue.Enqueue(task);
+        }
+
+        /// <summary>
+        /// Starts the thread in which execution of task happens..
+        /// </summary>
+        /// <param name="token"></param>
+        public void Execute()
+        {
+            ThreadPool.QueueUserWorkItem(state => {
+                while (true) { 
+                    Action taskToExecute;
+                    if (_tasksQueue.TryDequeue(out taskToExecute))
+                    {
+                        taskToExecute();
+                    }
+                    else
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+        }                   
+    }
+}
---
+++ TaskExecutor/MainClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace TaskExecutor
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Executor executor = new Executor();
+
+            //We start executor. It starts checking its execution queue and execute task if present
+            Console.WriteLine("Main Thread - Starting executor");
+
+            //Create threads, which will be adding tasks for execution
+            for (int i = 0; i < 15; i++)
+            {
+                Thread threadForTasks = new Thread(() =>
+                {
+                    Action toExecute = new SomeObject(Thread.CurrentThread.ManagedThreadId).ToExecute;
+                    executor.AddToExecutionQueue(toExecute);
+                });
+                Console.WriteLine("Task with ID {0} was added", threadForTasks.ManagedThreadId);
+                threadForTasks.Start();
+            }
+
+            executor.Execute();
+
+            Console.Read();
+        }
+        
+    }
+}
